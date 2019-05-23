c8aac181d5606608944ea70f36c0a0d0967ca22b
elorenzen <eric.lorenzen@gmail.com>
2019-05-23 11:47:37 -0600

    Passed faker.js data into comment container for image, name, date, and text

--- src/index.js
+++ src/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 
+
+
 const App = () => {
     return (
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    <img alt="avatar" src={faker.image.abstract()} />
                 </a>
                 <div className="content">
                     <a href="/" className="author">
-                        Eric
+                        {faker.name.firstName()}
                     </a>
                     <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
+                        <span className="date">
+                            Posted in {faker.date.month()}
+                        </span>
                     </div>
-                    <div className="text">Nice blog post!</div>
+                    <div className="text">{faker.lorem.sentences()}</div>
                 </div>
             </div>
         </div>
