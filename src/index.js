c3d773e81480376fbfe0268cf456f5b3f469a99a
elorenzen <eric.lorenzen@gmail.com>
2019-05-23 11:15:03 -0600

    Created index.js file with React and ReactDOM imports, basic App component setup and ReactDOM render

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>Basic component setup</div>
+    )
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
