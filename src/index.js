7082ac79bd0354a4a5661b1d7b2c78d02e153689
elorenzen <eric.lorenzen@gmail.com>
2019-05-23 11:31:00 -0600

    Created basic comment container component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     return (
-        <div>Basic component setup</div>
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Eric
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+        </div>
     )
 };
 
