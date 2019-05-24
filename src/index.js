4c87ae7cef3d9ad2a4e6c5133cc6440c6c6e2094
elorenzen <eric.lorenzen@gmail.com>
2019-05-24 12:40:53 -0600

    Passed avatar as prop

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author= {faker.name.firstName()} timeAgo={faker.date.month()} commentText={faker.lorem.sentences()} />
-            <CommentDetail author= {faker.name.firstName()} timeAgo={faker.date.month()} commentText={faker.lorem.sentences()} />
-            <CommentDetail author= {faker.name.firstName()} timeAgo={faker.date.month()} commentText={faker.lorem.sentences()} />
+            <CommentDetail author= {faker.name.firstName()} timeAgo={faker.date.month()} commentText={faker.lorem.sentences()} avatar={faker.image.avatar()} />
+            <CommentDetail author= {faker.name.firstName()} timeAgo={faker.date.month()} commentText={faker.lorem.sentences()} avatar={faker.image.avatar()} />
+            <CommentDetail author= {faker.name.firstName()} timeAgo={faker.date.month()} commentText={faker.lorem.sentences()} avatar={faker.image.avatar()} />
         </div>
     )
 };
