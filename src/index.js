0ebbfa3e9ff8179d30d2c8e5c00de5546d0b1c13
elorenzen <eric.lorenzen@gmail.com>
2019-05-24 13:03:02 -0600

    Imported ApprovalCard and passed component into container comments div

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard />
             <CommentDetail author= {faker.name.firstName()} timeAgo={faker.date.month()} commentText={faker.lorem.sentences()} avatar={faker.image.avatar()} />
             <CommentDetail author= {faker.name.firstName()} timeAgo={faker.date.month()} commentText={faker.lorem.sentences()} avatar={faker.image.avatar()} />
             <CommentDetail author= {faker.name.firstName()} timeAgo={faker.date.month()} commentText={faker.lorem.sentences()} avatar={faker.image.avatar()} />
